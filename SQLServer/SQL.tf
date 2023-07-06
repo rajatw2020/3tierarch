c290d9f39cdcece29e66953b14352dde337fbd8a
rajatw2020 <69303990+rajatw2020@users.noreply.github.com>
2023-07-07 01:16:50 +0530

    Update SQL.tf

--- SQLServer/SQL.tf
+++ SQLServer/SQL.tf
@@ -1,1 +1,70 @@
+resource "azurerm_sql_server" "primary" {
+    name = var.primary_database
+    resource_group_name = var.resource_group
+    location = var.location
+    version = var.primary_database_version
+    administrator_login = var.primary_database_admin
+    administrator_login_password = var.primary_database_password
+}
 
+resource "azurerm_sql_database" "db" {
+  name                = "db"
+  resource_group_name = var.resource_group
+  location            = var.location
+  server_name         = azurerm_sql_server.primary.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "db-nsg-subnet" {
+  subnet_id                 = var.db_subnet_id
+  network_security_group_id = azurerm_network_security_group.db-nsg.id
+}
+
+resource "azurerm_subnet" "db-subnet" {
+  name                 = "db-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  resource_group_name  = var.resource_group
+  address_prefixes     = [var.dbsubnetcidr]
+}
+
+
+resource "azurerm_network_security_group" "db-nsg" {
+    name = "db-nsg"
+    location = var.location
+    resource_group_name = var.resource_group
+
+    security_rule {
+        name = "ssh-rule-1"
+        priority = 101
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.2.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "3306"
+    }
+    
+    security_rule {
+        name = "ssh-rule-2"
+        priority = 102
+        direction = "Outbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.2.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "3306"
+    }
+    
+    security_rule {
+        name = "ssh-rule-3"
+        priority = 100
+        direction = "Outbound"
+        access = "Deny"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.1.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "3306"
+    }
+}
