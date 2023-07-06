0a4cffd7345b785ff6ee53b790cdc26faf934c63
rajatw2020 <69303990+rajatw2020@users.noreply.github.com>
2023-07-07 01:15:15 +0530

    Update Variables.tf

--- AppVirtualMachine/Variables.tf
+++ AppVirtualMachine/Variables.tf
@@ -1,1 +1,9 @@
+variable "resource_group" {}
+variable "location" {}
+variable "app_subnet_id" {}
+variable "app_host_name"{}
+variable "app_username" {}
+variable "app_os_password" {}
 
+variable "vnetcidr" {}
+variable "appsubnetcidr" {}
