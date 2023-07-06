41cca7efe0b22f6a1e7a0e8d51a614f1931b8218
rajatw2020 <69303990+rajatw2020@users.noreply.github.com>
2023-07-07 01:13:58 +0530

    Update AppVm.tf

--- AppVirtualMachine/AppVm.tf
+++ AppVirtualMachine/AppVm.tf
@@ -1,1 +1,94 @@
 
+ resource "azurerm_availability_set" "app_availabilty_set" {
+  name                = "app_availabilty_set"
+  location            = var.location
+  resource_group_name = var.resource_group
+ }
+
+resource "azurerm_subnet" "app-subnet" {
+  name                 = "app-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  resource_group_name  = var.resource_group
+  address_prefixes     = [var.appsubnetcidr]
+}
+
+resource "azurerm_network_interface" "app-net-interface" {
+    name = "app-network"
+    resource_group_name = var.resource_group
+    location = var.location
+
+    ip_configuration{
+        name = "app-webserver"
+        subnet_id = var.app_subnet_id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
+
+resource "azurerm_network_security_group" "app-nsg" {
+    name = "app-nsg"
+    location = var.location
+    resource_group_name = var.resource_group
+
+    security_rule {
+        name = "ssh-rule-1"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.1.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "22"
+    }
+    
+    security_rule {
+        name = "ssh-rule-2"
+        priority = 101
+        direction = "Outbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.1.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "22"
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "app-nsg-subnet" {
+  subnet_id                 = var.app_subnet_id
+  network_security_group_id = azurerm_network_security_group.app-nsg.id
+}
+
+resource "azurerm_virtual_machine" "app-vm" {
+  name = "app-vm"
+  location = var.location
+  resource_group_name = var.resource_group
+  network_interface_ids = [ azurerm_network_interface.app-net-interface.id ]
+  availability_set_id = azurerm_availability_set.web_availabilty_set.id
+  vm_size = "Standard_D2s_v3"
+  delete_os_disk_on_termination = true
+  
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name = "app-disk"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name = var.app_host_name
+    admin_username = var.app_username
+    admin_password = var.app_os_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
