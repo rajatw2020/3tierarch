974d1ba09e6eb9156f4c3fc03a42219ab53609a5
rajatw2020 <69303990+rajatw2020@users.noreply.github.com>
2023-07-07 01:19:19 +0530

    Update WebVM.tf

--- WebVirtualMachine/WebVM.tf
+++ WebVirtualMachine/WebVM.tf
@@ -1,1 +1,111 @@
 
+#Create Resource Group for the VM
+resource "azurerm_resource_group" ThreeTierApplication" {
+  name     = var.name
+  location = var.location
+}
+
+
+resource "azurerm_availability_set" "web_availabilty_set" {
+  name                = "web_availabilty_set"
+  location            = var.location
+  resource_group_name = var.resource_group
+}
+
+#Create the Vnet for the VMs to be connected
+resource "azurerm_virtual_network" "vnet01" {
+  name                = "vnet01"
+  resource_group_name = var.resource_group
+  location            = var.location
+  address_space       = [var.vnetcidr]
+}
+
+resource "azurerm_subnet" "web-subnet" {
+  name                 = "web-subnet"
+  virtual_network_name = azurerm_virtual_network.vnet01.name
+  resource_group_name  = var.resource_group
+  address_prefixes     = [var.websubnetcidr]
+}
+
+
+resource "azurerm_network_interface" "web-net-interface" {
+    name = "web-network"
+    resource_group_name = var.resource_group
+    location = var.location
+
+    ip_configuration{
+        name = "web-webserver"
+        subnet_id = var.web_subnet_id
+        private_ip_address_allocation = "Dynamic"
+    }
+}
+
+resource "azurerm_network_security_group" "web-nsg" {
+  name                = "web-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group
+  
+  security_rule {
+    name                       = "ssh-rule-1"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "22"
+  }
+  
+  security_rule {
+    name                       = "ssh-rule-2"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "Tcp"
+    source_address_prefix      = "192.168.3.0/24"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "22"
+}
+}
+
+resource "azurerm_subnet_network_security_group_association" "web-nsg-subnet" {
+  subnet_id                 = var.web_subnet_id
+  network_security_group_id = azurerm_network_security_group.web-nsg.id
+}
+
+resource "azurerm_virtual_machine" "web-vm" {
+  name = "web-vm"
+  location = var.location
+  resource_group_name = var.resource_group
+  network_interface_ids = [ azurerm_network_interface.web-net-interface.id ]
+  availability_set_id = azurerm_availability_set.web_availabilty_set.id
+  vm_size = "Standard_D2s_v3"
+  delete_os_disk_on_termination = true
+  
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name = "web-disk"
+    caching = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name = var.web_host_name
+    admin_username = var.web_username
+    admin_password = var.web_os_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+  
